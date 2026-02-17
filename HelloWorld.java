a89ffd165ed64956de10958c2046f15dad36f41a
brajendra <154320255+Brajbrishti@users.noreply.github.com>
2026-02-17 14:26:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 	public static void main(String[] args){
 	  System.out.println("*** Hello Java ****");
-	  
-		   System.out.println("*** thanks again ! ****");
+	  		int a=10,b=20,c
+				c=a+b
+				
+		   System.out.println(c);
 	}
     }
