974072a34e21c2a73e0bb8129742778f0b2ea4de
brajendra <154320255+Brajbrishti@users.noreply.github.com>
2026-02-19 18:34:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	  System.out.println("*** Hello Java ****");
 	  		int a=10,b=20,c;
 				c=a+b;
-				c=a*b;
+				
 		   System.out.println(c);
 			System.out.println("Thanks !!!!!!");
 	}
